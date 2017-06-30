8feeda6c9fcf76ca5df805e3c004cb6e7797a89a
mrtnzrm2 <csg1c12@gmail.com>
2017-06-30 13:48:43 -0500

    fresh versions of the files but stacking in the same problem

--- python/SimpleRalphabet.py
+++ python/SimpleRalphabet.py
@@ -9,33 +9,36 @@ import array
 import re
 
 class SimpleRalphabet:
-	def __init__(self , Set_Of_Input_Histograms , Axis1Min , Axis1Max , Axis2Formula="" , OutputName="test", name="pseudocat"):
+	def __init__(self , Set_Of_Input_Histograms , Axis1Min , Axis1Max , Axis2Formula="" , OutputName="test", name="ps"):
 		self.histograms = Set_Of_Input_Histograms
-		self.name = name
 		self.axis1min = Axis1Min
 		self.axis1max = Axis1Max
 		self.axis2for = Axis2Formula
 		self.output = OutputName
-		self.a1 = r.RooRealVar("Axis1_"+self.name,"a1_"+self.name, self.axis1min, self.axis1max)
+		self.a1 = r.RooRealVar("Axis1_"+name,"a1_"+name, self.axis1min, self.axis1max)
 		outputfile = r.TFile('%s.root'%(OutputName),"recreate")
-		workspace = r.RooWorskspace("test")
-		category = r.RooCategory("sample","sample")
+		workspace = r.RooWorkspace("w","workspace")
+		#category = r.RooCategory("sample","sample")
 		H = self.simpleRalphabet()
 		self.setWorkspace(H,workspace)
 
 	def simpleRalphabet(self , name="pseudocat"):
 		rooH=[]
-		for h,n in (self.histograms,len(self.histograms)):
-			min = h.GetXaxis().GetBinLowEdge(1)
-			max = h.GetXaxis().GetBinUpEaaadge(h.GetXaxis().GetNbins())
-			self.a2 = r.RooRealVar("Axis2_"+name+str(n), "a2_"+name+str(n), min, max)
-			fH = self.createRooParametricHist("RooParametricHist_"+name+str(n),"RooParametricHist_"+name+str(n), self.a1 , r.RooArgList(self.a2) , h)
+		min_ = self.histograms[0].GetXaxis().GetBinLowEdge(1)
+		max_ = self.histograms[0].GetXaxis().GetBinUpEdge(self.histograms[0].GetXaxis().GetNbins())
+		self.a2 = r.RooRealVar("Axis2_"+name, "a2_"+name, min_, max_)
+		
+		for h,n in zip(self.histograms,range(len(self.histograms))):
+
+			#print type(r.RooArgList(self.a2) )
+			fH = r.RooParametricHist("RooParametricHist_"+name+str(n),"RooParametricHist_"+name+str(n), self.a1 , r.RooArgList(self.a2) , h)
 			if self.axis2for is not "":
                         	DecorVar = r.RooFormulaVar("DecoVar_"+name, self.axis2for, r.RooArgList(self.a1,self.a2))
 				fH = r.RooParametricHist("RooParametricHist_"+name+str(n),"RooParametricHist_"+name+str(n), self.a1 , r.RooArgList(DecorVar), h)
 			rooH.append(fH)
 		return rooH
-	def setWorkspace(self, lHis, workspace , name="workspace"):
+	def setWorkspace(self, lHis, workspace):
 		for lh in lHis:
-			getattr(workspace,'import')(lh,r.RooFit.RecycleConflictNodes())	
-		workspace.writeToFile(self.output+"_"+name)
+			getattr(workspace,'import')(lh)	
+			#workspace.import(lh)
+		workspace.writeToFile(self.output++"_.root")
--- python/example.py
+++ python/example.py
@@ -2,17 +2,21 @@ from SimpleRalphabet import SimpleRalphabet
 import ROOT as r
 
 
-h1 = r.TH2F("a","a",50,0,100,5,200,1000)
-h2 = r.TH2F("b","b",50,0,100,5,200,1000)
-h3 = r.TH2F("c","c",50,0,100,5,200,1000)
-h4 = r.TH2F("d","d",50,0,100,5,200,1000)
+#h1 = r.TH2F("a","a",50,0,100,5,200,1000)
+#h2 = r.TH2F("b","b",50,0,100,5,200,1000)
+#h3 = r.TH2F("c","c",50,0,100,5,200,1000)
+#h4 = r.TH2F("d","d",50,0,100,5,200,1000)
+h1 = r.TH1F("a","a",50,0,100)
+h2 = r.TH1F("b","b",50,0,100)
+h3 = r.TH1F("c","c",50,0,100)
+h4 = r.TH1F("d","d",50,0,100)
 H=[h1,h2,h3,h4]
 pt=0.
 m=0.
 for h in H:
 	for i in range(25000):
-		r.gRandom.Rannor(pt,m)
-		h.Fill(pt,4*m)
+		r.gRandom.Rannor(r.Double(pt),r.Double(m))
+		h.Fill(pt)
 
 SimpleRalphabet(H,0,100,"@0+@1")
 
